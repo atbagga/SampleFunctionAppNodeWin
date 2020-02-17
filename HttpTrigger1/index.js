9de27d84ec7aef9bc04e324e37c2e64ee720efc7
Atul Bagga <atbagga@microsoft.com>
2020-02-17 17:11:51 +0530

    Update index.js

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -4,7 +4,7 @@
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello123" + (req.query.name || req.body.name)
         };
     }
     else {
